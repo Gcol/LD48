44205e846eecdf1d22023625dfaaf0d3662ff739
leoBG <leobgely@gmail.com>
2021-04-25 18:18:56 +0200

    Generation Nivo

---
+++ LD48/Assets/Scripts/Nivo/Nivo.cs
@@ -0,0 +1,67 @@
+    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nivo : MonoBehaviour
+{
+
+    static private Nivo cela;
+
+    static public Nivo Instance
+    {
+        get
+        {
+            if (!cela) cela = FindObjectOfType<Nivo>();
+            return cela;
+        }
+    }
+
+    [SerializeField] private float delaiSpawnSection;
+    [SerializeField] private Section prefabSection;
+    private int nbrSections;
+    [SerializeField] private Gradient gradient;
+
+    private void Awake()
+    {
+        //InitSections();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RoutineSpawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+
+    private IEnumerator RoutineSpawn()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(delaiSpawnSection);
+            
+            if(Instantiate(prefabSection, transform).TryGetComponent(out Section section))
+            {
+                section.transform.localScale = new Vector3(0, 0, 1);
+                section.sprRend.color = gradient.Evaluate(Random.Range(0f, 1f));
+                section.sprRend.sortingOrder = nbrSections;
+                nbrSections++;
+            }
+        }
+    }
+
+    public void ChangerGradientGenerationSection(Gradient nvgradient)
+    {
+        gradient = nvgradient;
+    }
+}
---
+++ LD48/Assets/Scripts/Nivo/testcircle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testcircle : MonoBehaviour
+{
+    // avec r le rayon max du cercle et t le temps entre lecercle qui est un point et sa taille max, r*(t^2)
+    [SerializeField] private SpriteRenderer srpRend;
+    [SerializeField] float vitesse = 10; //in second
+    [SerializeField] float forceRayon = 1.2f;
+    [SerializeField] float rayonMax = 40;
+    int frames = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frames++;
+        transform.localScale = new Vector3 (forceRayon * (Mathf.Pow(frames,2))/(vitesse* Time.deltaTime), forceRayon * (Mathf.Pow(frames, 2)) / (vitesse* Time.deltaTime), 1f);
+
+        if (transform.localScale.x > rayonMax) Destroy(gameObject);
+    }
+
+
+}
