f40f64c92be3dd18cdab2dc426108e49e02e8e35
leoBG <leobgely@gmail.com>
2021-04-26 20:16:23 +0200

    Léo en peut plus

--- LD48/Assets/Scripts/TomberProfond.cs
+++ LD48/Assets/Scripts/TomberProfond.cs
@@ -25,9 +25,9 @@ public class TomberProfond : MonoBehaviour
 
     private void Tomber()
     {
-        transform.position = Vector3.Lerp(positionDepart, Vector3.zero, temps);
+        transform.position = Vector3.Lerp(positionDepart, Vector3.zero - Vector3.forward, temps);
         transform.localScale = Vector3.Lerp(tailleDepart, Vector3.zero, temps);
-        if (temps > 1) Destroy(gameObject);
         transform.localEulerAngles += Vector3.forward * 10 * Time.deltaTime;
+        if (temps > 1) Destroy(gameObject);
     }
 }
