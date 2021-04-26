010b4c91cb06bf4f5d26cb40f723e40646e6660e
leoBG <leobgely@gmail.com>
2021-04-26 18:04:00 +0200

    Menus Intégrés (sauf les crédits)

--- LD48/Assets/Scripts/GameManager.cs
+++ LD48/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -16,19 +17,25 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    [SerializeField] private GameObject menuPause;
     [SerializeField] private Nivo nivo;
     [SerializeField] private GestionnaireEvenement gestionnaireEvenement;
+
+    private bool enPause = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        menuPause.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            MettreEnPause(!enPause);
+        }
     }
 
     public void CommencerJeu()
@@ -37,6 +44,27 @@ public class GameManager : MonoBehaviour
         gestionnaireEvenement.Init();
     }
     
-    
+    public void MettreEnPause(bool pause)
+    {
+        enPause = pause;
+        Time.timeScale = pause ? 0 : 1;
+        menuPause.SetActive(pause);
+    }
 
+    public void RelancerJeu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void Quitter()
+    {
+#if (UNITY_EDITOR)
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif (UNITY_STANDALONE)
+            Application.Quit();
+#elif (UNITY_WEBGL)
+            Application.OpenURL("https://www.youtube.com/watch?v=zXvTfca7i6A");
+#endif
+    }
 }
